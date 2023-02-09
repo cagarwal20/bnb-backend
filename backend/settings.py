86c0ed080ae35e2b45f7ef71b950090c828bab1e
Chetan <cagarwal20@gmail.com>
2023-02-09 19:24:35 +0530

    added db auth

--- backend/settings.py
+++ backend/settings.py
@@ -84,9 +84,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': "postgres",    
+        "USER": "postgres",
+        "PASSWORD": "bnb-database-prod",
+        "HOST": "db.ykphrmxzsvzzubalbocm.supabase.co",
+        "PORT": 5432,
+        }
     }
-}
+
 # settings.py
 
 
